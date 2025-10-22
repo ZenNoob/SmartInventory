e3210f8d8b436661b1f604116e7d286fcf64ad4d
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-22 05:58:42 +0000

    ý  tôi là hiện tại trang danh mục sản phẩm bị gom về bên trái màn hình đ

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -216,7 +216,7 @@ const Sidebar = React.forwardRef<
       <div
         ref={ref}
         className="group peer hidden md:block text-sidebar-foreground"
-        data-state={state}
+        data-sidebar-state={state}
         data-collapsible={state === "collapsed" ? collapsible : ""}
         data-variant={variant}
         data-side={side}
@@ -303,7 +303,7 @@ const SidebarRail = React.forwardRef<
       className={cn(
         "absolute inset-y-0 z-20 hidden w-4 -translate-x-1/2 transition-all ease-linear after:absolute after:inset-y-0 after:left-1/2 after:w-[2px] hover:after:bg-sidebar-border group-data-[side=left]:-right-4 group-data-[side=right]:left-0 sm:flex",
         "[[data-side=left]_&]:cursor-w-resize [[data-side=right]_&]:cursor-e-resize",
-        "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
+        "[[data-side=left][data-sidebar-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-sidebar-state=collapsed]_&]:cursor-w-resize",
         "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full group-data-[collapsible=offcanvas]:hover:bg-sidebar",
         "[[data-side=left][data-collapsible=offcanvas]_&]:-right-2",
         "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
@@ -327,6 +327,7 @@ const SidebarInset = React.forwardRef<
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background transition-all",
         !isMobile && state === 'collapsed' && 'md:pl-[--sidebar-width-icon]',
+        !isMobile && state === 'expanded' && 'md:pl-[--sidebar-width]',
         className
       )}
       {...props}
