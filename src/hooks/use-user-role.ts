942288d72608920859404784754e3f394fcb758e
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-10-22 02:13:41 +0000

    vẫn chưa thấy menu quản lý tài khoản

--- src/hooks/use-user-role.ts
+++ src/hooks/use-user-role.ts
@@ -1,12 +1,17 @@
 'use client';
 
-import { useDoc, useFirestore, useUser } from "@/firebase";
-import { doc, query } from "firebase/firestore";
+import { useDoc, useFirestore, useUser, useMemoFirebase } from "@/firebase";
+import { doc } from "firebase/firestore";
 
 export function useUserRole() {
   const { user } = useUser();
   const firestore = useFirestore();
-  const userDocRef = user ? doc(firestore, 'users', user.uid) : null;
+  
+  const userDocRef = useMemoFirebase(() => {
+    if (!user || !firestore) return null;
+    return doc(firestore, 'users', user.uid);
+  }, [user, firestore]);
+
   const { data: userProfile } = useDoc(userDocRef);
 
   return { role: userProfile?.role };
