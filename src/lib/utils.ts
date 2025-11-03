dc0d97e8ff91b4cda1a4ec1a16be4f3486e745c1
Nguyen Minh Phuc <phucteacher2019@gmail.com>
2025-11-03 08:22:58 +0000

    Nếu làm vậy khi in ra lại bị mất đơn vị tiền tệ. hoặc bỏ luôn phần đơn v

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,7 +10,7 @@ export function formatCurrency(amount: number) {
   return new Intl.NumberFormat('en-US', {
     style: 'decimal',
     maximumFractionDigits: 0,
-  }).format(amount) + '\u00A0VNĐ';
+  }).format(amount);
 }
 
 /**
